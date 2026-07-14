2104b12df434684dc8fba5ef6228880dacc8ee09
Shilpa Mantur <Shilpa Mantur@Shilpa_Mantur>
2026-07-14 16:18:15 +0530

    First Commit

---
+++ TNSIF_Training/src/org/tnsif/acce/c2tc/java/HelloWorld.java
@@ -0,0 +1,11 @@
+package org.tnsif.acce.c2tc.java;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Hello World");
+
+	}
+
+}
