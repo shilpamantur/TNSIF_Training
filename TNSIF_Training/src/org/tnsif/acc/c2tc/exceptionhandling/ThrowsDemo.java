663aa7b803925b2837cc6618b822799440cf5d7c
Shilpa Mantur <Shilpa Mantur@Shilpa_Mantur>
2026-08-27 21:43:12 +0530

    first commit

--- TNSIF_Training/src/org/tnsif/acc/c2tc/exceptionhandling/ThrowsDemo.java
+++ TNSIF_Training/src/org/tnsif/acc/c2tc/exceptionhandling/ThrowsDemo.java
@@ -1,10 +1,49 @@
 package org.tnsif.acc.c2tc.exceptionhandling;
 
+import java.io.IOException;
+
 public class ThrowsDemo {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	void print()
+	{
+		System.out.println("Hello World");
+	}
+	void fileReading() throws IOException
+	{
+		System.out.println("File not Found");
+	}
+	void Division() throws ArithmeticException ,ClassNotFoundException,IOException
+	{
+		System.out.println("Can't divide by zero");
+	}
 
+	public static void main(String[] args) {
+		ThrowsDemo obj=new ThrowsDemo();
+	     obj.print();
+	     
+	     
+	     try {
+	    	 obj.fileReading();
+	     }catch(IOException e)
+	     {
+	    	 System.out.println(e);
+	     }
+	     
+	     try {
+	    	 obj.Division();
+	     }catch(ArithmeticException e)
+	     {
+	    	 System.out.println(e);
+	     }catch(ClassNotFoundException e)
+	     {
+	    	 System.out.println(e);
+	     }
+	     catch(IOException e)
+	     {
+	    	 System.out.println(e);
+	     }
+	      
+	     
 	}
 
 }
