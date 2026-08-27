34757c757dbf328489a21ea4a66349e084a65c98
Shilpa Mantur <Shilpa Mantur@Shilpa_Mantur>
2026-08-27 20:05:55 +0530

    first commit

--- TNSIF_Training/src/org/tnsif/acc/c2tc/collectionsdemo/ArrayListDemo.java
+++ TNSIF_Training/src/org/tnsif/acc/c2tc/collectionsdemo/ArrayListDemo.java
@@ -6,40 +6,37 @@ import java.util.List;
 public class ArrayListDemo {
 
 	public static void main(String[] args) {
-		List<String> fruits=new ArrayList<>();
+
+		List<String> fruits = new ArrayList<>();
+
 		fruits.add("Apple");
 		fruits.add("Mango");
 		fruits.add("Dragon");
-        
+
 		System.out.println(fruits);
-		
-		System.out.println("Second Fruit :"+fruits.get(1));
-		
+
+		System.out.println("Second Fruit :" + fruits.get(1));
+
 		fruits.set(1, "Cherry");
 		System.out.println(fruits);
-		
-		fruits.remove(2);
-		System.out.println("After removing "+fruits);
-		
-		
-		if(fruits.contains("Mango"))
-		{
-			System.out.println("Requested fruit is present");
-		}else
-		{
-			System.out.println("Requested fruit is not prsent");
-		}
-		
-	
-        Iterable<String> iterableFruits = fruits;
 
-        System.out.println("Traversing using Iterable:"+fruits);
-        
-        //using for-each loop also you can traverse
-		for(String fruit:fruits)
-		{
-			System.out.println("Traversing using for-each loop "+fruit);
+		fruits.remove(2);
+		System.out.println("After removing " + fruits);
+
+		if (fruits.contains("Mango")) {
+			System.out.println("Requested fruit is present");
+		} else {
+			System.out.println("Requested fruit is not present");
+		}
+
+		// Using Iterable
+		Iterable<String> iterableFruits = fruits;
+
+		System.out.println("Traversing using Iterable:" + iterableFruits);
+
+		// Using for-each loop
+		for (String fruit : iterableFruits) {
+			System.out.println("Traversing using for-each loop " + fruit);
 		}
 	}
-
 }
