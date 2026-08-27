a306ceb8ee17f78c6ba8db92092c85020e12b648
Shilpa Mantur <Shilpa Mantur@Shilpa_Mantur>
2026-08-27 19:59:38 +0530

    first commit

--- TNSIF_Training/src/org/tnsif/acc/c2tc/datatypes_typecasting/ShoppingCart.java
+++ TNSIF_Training/src/org/tnsif/acc/c2tc/datatypes_typecasting/ShoppingCart.java
@@ -3,15 +3,21 @@ package org.tnsif.acc.c2tc.datatypes_typecasting;
 public class ShoppingCart {
 
 	public static void main(String[] args) {
-		//Implicit (smaller-larger)
-		
-		int quantity=3;
-		double pricePerItem=99.50d;
-		double total_price=quantity*pricePerItem;  //int->double automatically
-		
-		double discount=10.75;
-		
 
+		// Implicit Type Casting (smaller -> larger)
+
+		int quantity = 3;
+
+		double pricePerItem = 99.50;
+
+		// int -> double automatically
+		double total_price = quantity * pricePerItem;
+
+		double discount = 10.75;
+
+		System.out.println("Quantity: " + quantity);
+		System.out.println("Price per Item: " + pricePerItem);
+		System.out.println("Total Price: " + total_price);
+		System.out.println("Discount: " + discount);
 	}
-
 }
